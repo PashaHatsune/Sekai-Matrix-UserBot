b18486eef5cb2f77d32a3ce8c37a126c4b4b9ba9
PashaHatsune <39mkui@gmail.com>
2026-04-16 18:48:47 +0300

    allowed_symbols теперь в ConfigValue

--- src/mxuserbot/modules/core/set_prefix.py
+++ src/mxuserbot/modules/core/set_prefix.py
@@ -5,37 +5,43 @@ from ...core import loader, utils
 class Meta:
     name = "PrefixModule"
     _cls_doc = "Управление префиксом команд юзербота."
-    version = "1.0.0"
+    version = "1.0.1"
     tags = ["settings"]
 
 
 @loader.tds
 class PrefixModule(loader.Module):
+    config = {
+        "allowed_symbols": loader.ConfigValue(default="!\"./\\,;:@#$%^&*-_+=?|~", description="list allowed symbols"),
+    }
+
     strings = {
-        "allowed_symbols": "!\"./\\,;:@#$%^&*-_+=?|~",
-        "error_no_args": "<b>Ошибка:</b> вы не указали префикс.<br>Пример: <code>.set_prefix !</code>",
-        "error_too_long": "<b>Ошибка:</b> префикс должен состоять только из <b>одного</b> символа.",
-        "error_set_prefix": "<b>Ошибка:</b> символ <code>{new_prefix}</code> запрещен.<br>"
-                            "Можно использовать только: <code>{allowed_symbols}</code>",
-        "success_set_prefix": "✅ Префикс успешно изменен на: <code>{new_prefix}</code>"
+        "error_no_args": "❌ | <b>No prefix specified.</b><br>Example: <code>.set_prefix !</code>",
+        "error_too_long": "❌ | <b>The prefix must be exactly <u>one</u></b> character long.",
+        "error_set_prefix": "❌ | <b>The character <code>{new_prefix}</code> is not allowed.</b><br>"
+                            "You can only use: <code>{allowed_symbols}</code>",
+        "success_set_prefix": "✅ | <b>Prefix successfully changed to</b>: <code>{new_prefix}</code>"
     }
 
     @loader.command()
-    async def set_prefix(self, mx: Any, event: MessageEvent):
+    async def set_prefix(self, mx, event: MessageEvent):
         """Установить новый префикс (только спец. символы)"""
-        
-        body = getattr(event.content, "body", "")
-        parts = body.split()
-        
-        if len(parts) < 2:
+
+        args = await utils.get_args(
+            mx=mx,
+            event=event
+        )
+    
+        if len(args) < 1:
             return await utils.answer(mx, self.strings.get("error_no_args"))
 
-        new_prefix = parts[1]
+        new_prefix = args[0]
 
         if len(new_prefix) != 1:
             return await utils.answer(mx, self.strings.get("error_too_long"))
 
-        allowed = self.strings.get("allowed_symbols")
+        allowed = self.config.get("allowed_symbols")
+
         if new_prefix not in allowed:
             return await utils.answer(mx, 
                 self.strings.get("error_set_prefix").format(
